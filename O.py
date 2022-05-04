84daf0d87fac6a36b6e2993547311f53fd7a4d9f
4ist <fpailes@DESKTOP-T0Q4PSP.localdomain>
2022-05-04 14:17:17 -0600

    created O and X classes

---
+++ O.py
@@ -0,0 +1,6 @@
+class O
+    def __init__(self, positionX, positionY):
+        positionX = self.positionX
+        positionY = self.positionY
+    def __repr__(self)
+        print(str(positionX) +" "+ str(positionY))
---
+++ X.py
@@ -0,0 +1,11 @@
+class X:
+
+    def __init__(self, positionX, PositionY):
+
+        positionX = self.positionX
+        positionY = self.positionY
+
+    def __refr__(self):
+        print(str(self.positionX) + " " + str(self.positionY))
+
+
